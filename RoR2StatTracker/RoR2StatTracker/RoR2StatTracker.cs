a950d7122d6d96b46b2874adb39d7f43a98b6f67
LeoneShamoth <nehnehpwn@gmail.com>
2019-04-29 00:09:30 -0400

    I honestly cant tell if I am an idiot or not, but this SHOULD work. I will be able to construct my own, simpler, inventory with this. I dont know how to pull the original inventory in all honesty.

---
+++ RoR2StatTracker/RoR2StatTracker/RoR2StatTracker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using BepInEx;
+using RoR2;
+
+namespace RoR2StatTracker
+{
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("com.LeoneShamoth.RoR2StatTracker", "Risk of Rain 2 Stat Tracker", "1.0.0")]
+
+    public class RoR2StatTracker : BaseUnityPlugin
+    {
+        private int[] currentInventory = new int[78];
+        public void Awake()
+        {
+            Chat.AddMessage("Loaded Risk of Rain 2 Stat Tracker!");
+            On.RoR2.Inventory.GiveItem += CatchItemGained;
+            On.RoR2.Inventory.RemoveItem += CatchItemRemoved;
+
+        }
+
+        private void CatchItemGained(On.RoR2.Inventory.orig_GiveItem orig, Inventory self, ItemIndex itemIndex, int count)
+        {
+            orig.Invoke(self, itemIndex, count);
+
+            if ((int)itemIndex != 47 && (int)itemIndex != 48)
+            {
+                Chat.AddMessage("Item #: " + (int)itemIndex + " Item Count: " + count);
+            }
+        }
+
+        private void CatchItemRemoved(On.RoR2.Inventory.orig_RemoveItem orig, Inventory self, ItemIndex itemIndex, int count)
+        {
+            orig.Invoke(self, itemIndex, count);
+
+            if ((int)itemIndex != 47 && (int)itemIndex != 48)
+            {
+                Chat.AddMessage("Item #: " + (int)itemIndex + " Item Count: " + (-count));
+            }
+        }
+    }
+}
